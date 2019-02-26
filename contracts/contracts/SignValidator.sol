304fcb46bb84ecdb9316ab33e87fb2def39a509f
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2019-02-26 12:41:39 +0900

    remove unnecessary destination

--- contracts/contracts/SignValidator.sol
+++ contracts/contracts/SignValidator.sol
@@ -7,12 +7,11 @@ contract SignValidator {
         uint8 sigV,
         bytes32 sigR,
         bytes32 sigS,
-        address destination,
         bytes memory data, // original message
         address originalSigner
     ) public {
         // EIP191 convertible.
-        bytes32 h = keccak256(abi.encodePacked(byte(0x19), byte(0x45), address(this), originalSigner, nonce[originalSigner], destination, data));
+        bytes32 h = keccak256(abi.encodePacked(byte(0x19), byte(0x45), address(this), originalSigner, nonce[originalSigner], data));
         address addressFromSig = ecrecover(h, sigV, sigR, sigS);
 
         require(addressFromSig == originalSigner);
